cd195f8fee9f9fe15764dd2f2d35ee699bc4f0e4
Cristian Mejia <cristianmejia00@hotmail.com>
2017-06-13 17:39:58 +0900

    Create functions.R
    Corrected input variable

--- R/functions.R
+++ R/functions.R
@@ -7,7 +7,7 @@
 #' @examples
 #' keyword_to_list(a_data_frame)
 keywords_to_list <- function(a_data_frame) {
-  serie <- seq(1, ncol(keywords)-1, 2)
+  serie <- seq(1, ncol(a_data_frame)-1, 2)
   lodf <- lapply(serie, function(x){
     term <- a_data_frame[, x, with=FALSE]
     relevance <- a_data_frame[, x+1, with=FALSE]

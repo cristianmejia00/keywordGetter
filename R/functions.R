5715efef6b60c8ecdf365111d6ad5701a46195f8
cristianmejia00 <cristianmejia00@hotmail.com>
2017-06-13 17:31:57 +0900

    First commit. Upload the package

---
+++ R/functions.R
@@ -0,0 +1,17 @@
+#' Transform topic model reports to the input object for heatmaps
+#'
+#' Transform topic model reports to the input object for heatmaps
+#' @param a_data_frame a dataframe created with topic_model_5.R
+#' @keywords read, topic models
+#' @export
+#' @examples
+#' keyword_to_list(a_data_frame)
+keywords_to_list <- function(a_data_frame) {
+  serie <- seq(1, ncol(keywords)-1, 2)
+  lodf <- lapply(serie, function(x){
+    term <- a_data_frame[, x, with=FALSE]
+    relevance <- a_data_frame[, x+1, with=FALSE]
+    return(data.frame(term, relevance))
+  })
+  return(lodf)
+}
